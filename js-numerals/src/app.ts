4361b596927f1f0d07cc466e04d403d5ac856ab2
Norbert Gresku <norbirnr@gmail.com>
2019-01-11 23:11:29 +0100

    adding basic markup

--- js-numerals/src/app.ts
+++ js-numerals/src/app.ts
@@ -1,3 +1,12 @@
+const submit = document.querySelector('button');
+const input = document.querySelector('input');
+const result = document.querySelector('.result-text');
+
+submit.addEventListener('click', () => {
+  console.log(typeof input.value);
+  console.log(addPostfix(convertToString(input.value)).join(' '));
+});
+
 const DIGITS_LT_21 = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty'];
 const ROUND_DOUBLES = [null, null, 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 const POSTFIX = [null, 'hundred', 'thousand', 'million', 'billion', 'trillion'];
